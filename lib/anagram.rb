5116f3b3aa1a944e6bdf28bbe2243a32cf7f9069
David Forer <david.j.forer@gmail.com>
2019-03-09 13:33:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,13 +2,13 @@
 class Anagram
   attr_accessor :word 
   
-  def initalize(word)
+  def initialize(word)
     @word = word
   end
   
   def match(array)
-    array.select do |the_word|
-      the_word.split("").sort == @word.split("").sort
+    array.select do |a_word|
+      a_word.split("").sort == @word.split("").sort
   end
 end
 end
